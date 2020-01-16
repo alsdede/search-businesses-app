974cb3bf17c9f5f1f5ce5b0aa62284d2cebfff0f
alsdede <andreluizsilveira.88@gmail.com>
2020-01-16 14:34:20 -0300

    add Readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+# search-businesses-app
+App Search Businesses with Yelp API
+
+Instalação e execução
+
+1. Faça um clone desse repositório;
+2. Entre na pasta do projeto;
+3. Rode `yarn` para instalar as dependências;
+4. Rode `yarn start` para iniciar o frontend.
--- src/screens/BusinessesShowScreen.js
+++ src/screens/BusinessesShowScreen.js
@@ -1,7 +1,7 @@
 import React,{ useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
-
+import { Rating, AirbnbRating } from 'react-native-elements';
 const BusinessesShowScreen = ({ navigation }) => {
     const [ business, setBusiness ] = useState(null);
     const idBusinesses = navigation.getParam('id');
@@ -19,21 +19,43 @@ const BusinessesShowScreen = ({ navigation }) => {
         return null;
     }
     return (
-        <View>
-        <Text>{business.name}</Text>
+        
+        <View>        
         <FlatList 
+        horizontal
         data={business.photos}
         keyExtractor={(photo)=> photo}
         renderItem={({item}) => 
         <Image style={styles.imageBusiness}source={{ uri: item }}/>}/>
+        <Text style={styles.titleBusiness}>{business.name}</Text>     
+        <Rating
+        type='star'
+        imageSize={30}
+        readonly
+        startingValue={business.rating}
+        style={styles.rating}      
+        />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+   
     imageBusiness:{
+        
         height: 200,
         width:300,
+        marginLeft:15,
+        marginBottom:30,
+    },
+    titleBusiness:{
+        fontSize:30,
+        marginLeft:15,
+    },
+    rating:{
+        marginRight:200,
+        paddingLeft:10 ,
+        
     }
 });
 
